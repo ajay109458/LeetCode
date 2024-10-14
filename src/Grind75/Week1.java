88b0a00d8cf6b801f9a3823a2669cb19d5d11455
ajayyadavmscs <ajayyadav@utexas.edu>
2024-10-14 23:18:54 +0530

    Leetcode Question #125

--- src/Grind75/Week1.java
+++ src/Grind75/Week1.java
@@ -160,4 +160,30 @@ public class Week1 {
 
         return maxProfit;
     }
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+            while(left < s.length() && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while(right >= 0 && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return  false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
 }
