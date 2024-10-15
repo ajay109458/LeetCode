92c91b97a0165879ad10bb206fe32f0a2b960d69
ajay109458 <ajay.yadav109458@gmail.com>
2024-10-15 15:42:40 +0530

    Leetcode Questions

--- src/LeetcodeBoard/LeetcodeBoard.java
+++ src/LeetcodeBoard/LeetcodeBoard.java
@@ -1,12 +1,7 @@
 package LeetcodeBoard;
 
 import utils.ListNode;
-
-import java.io.CharArrayReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class LeetcodeBoard {
 
@@ -132,7 +127,93 @@ public class LeetcodeBoard {
         return maxLen;
     }
 
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
 
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            int first = nums[i];
+            int targetSum = -1 * first;
+
+            int left = i+1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int currentSum = nums[left] + nums[right];
+
+                if (currentSum == targetSum) {
+                    List<Integer> tuple = Arrays.asList(nums[i], nums[left], nums[right]);
+                    result.add(tuple);
+
+                    while(i+1 < nums.length && nums[i] == nums[i+1]) {
+                        i++;
+                    }
+
+                    while(left < right && nums[left] == nums[left+1]) {
+                        left++;
+                    }
+
+                    while(left < right && nums[right] == nums[right-1]) {
+                        right--;
+                    }
+
+
+                    left++;
+                    right--;
+                } else if (currentSum < targetSum) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int trap(int[] height) {
+        int n = height.length;
+
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = height[0];
+        for(int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        }
+
+        rightMax[n-1] = height[n-1];
+        for(int i = n - 1; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+
+        int maxWater = 0;
+
+        for(int i = 0; i < n; i++) {
+            maxWater += (Math.min(leftMax[i], rightMax[i]) - height[i]);
+        }
+
+        return maxWater;
+    }
+
+    public int reverse(int x) {
+        boolean isNegative = false;
+
+        if (x < 0) {
+            x = -1 * x;
+            isNegative = true;
+        }
+
+        int rev = 0;
+
+        while(x != 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return isNegative ? -1 * rev : rev;
+    }
 
 
 }
