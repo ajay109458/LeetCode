e93948066265faab05ff7bde8e6a08b7644290df
ajay109458 <ajay.yadav109458@gmail.com>
2024-10-15 09:37:06 +0530

    Leetcode Question #242

---
+++ src/LeetcodeBoard/LeetcodeBoard.java
@@ -0,0 +1,115 @@
+package LeetcodeBoard;
+
+import utils.ListNode;
+
+import java.io.CharArrayReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LeetcodeBoard {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode rHead = null;
+        ListNode r = null;
+
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return null;
+        }
+
+        int carry = 0;
+
+        while(l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+
+            ListNode temp = new ListNode(sum);
+
+            if (rHead == null) {
+                rHead = temp;
+                r = rHead;
+            } else {
+                r.next = temp;
+                r = r.next;
+            }
+
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while(l1 != null) {
+            int sum = l1.val + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+            ListNode temp = new ListNode(sum);
+
+            if (rHead == null) {
+                rHead = temp;
+                r = rHead;
+            } else {
+                r.next = temp;
+                r = r.next;
+            }
+
+            l1 = l1.next;
+        }
+
+        while(l2 != null) {
+            int sum = l2.val + carry;
+            carry = sum/10;
+            sum = sum % 10;
+
+
+            ListNode temp = new ListNode(sum);
+
+            if (rHead == null) {
+                rHead = temp;
+                r = rHead;
+            } else {
+                r.next = temp;
+                r = r.next;
+            }
+
+            l2 = l2.next;
+        }
+
+        if (carry != 0) {
+            ListNode temp = new ListNode(carry);
+
+            if (rHead == null) {
+                rHead = temp;
+                r = rHead;
+            } else {
+                r.next = temp;
+                r = r.next;
+            }
+        }
+
+        return rHead;
+    }
+
+    public int maxSubArray(int[] nums) {
+        int maxSumSoFar = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for(int num : nums) {
+            currSum += num;
+            maxSumSoFar = Math.max(maxSumSoFar, currSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        return maxSumSoFar;
+    }
+
+
+
+
+}
