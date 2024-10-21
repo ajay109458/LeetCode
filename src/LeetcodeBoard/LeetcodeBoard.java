fcd3232e78a4ee5dfb7a2ca3eef6bef20b24e87b
ajay109458 <ajay.yadav109458@gmail.com>
2024-10-21 20:35:50 +0530

    Leetcode Questions

--- src/LeetcodeBoard/LeetcodeBoard.java
+++ src/LeetcodeBoard/LeetcodeBoard.java
@@ -1085,4 +1085,69 @@ public class LeetcodeBoard {
         return count;
     }
 
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+
+        Stack<int[]> stack = new Stack<>();
+        stack.push(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            while(!stack.isEmpty() && stack.peek()[1] > intervals[i][0]) {
+                int[] temp = stack.pop();
+                intervals[i][0] = Math.min(temp[0], intervals[i][0]);
+                intervals[i][1] = Math.max(intervals[i][1], temp[1]);
+            }
+
+            stack.push(intervals[i]);
+        }
+
+
+        int[][] res = new int[stack.size()][2];
+
+        for(int i = stack.size()-1; i >= 0; i--) {
+            res[i] = stack.pop();
+        }
+
+        return res ;
+    }
+
+    public String intToRoman(int num) {
+
+
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "I");
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+        map.put(100, "C");
+        map.put(400, "CD");
+        map.put(500, "D");
+        map.put(900, "CM");
+        map.put(1000, "M");
+
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+
+        int index = 0;
+
+        while(num > 0) {
+            int key = keys.get(index);
+            if (num >= key) {
+                sb.append(map.get(key));
+                num -= key;
+            } else {
+                index++;
+            }
+        }
+
+        return sb.toString();
+    }
 }
